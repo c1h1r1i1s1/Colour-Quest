79cf23c7fc69e80b3122aff5dbacc20881338cd0
c1h1r1i1s1 <lolasocf@gmail.com>
2024-10-17 18:15:09 +1100

    tts file

---
+++ src/textToSpeech.cpp
@@ -0,0 +1,4 @@
+// void textToSpeech(String textInput="Nice Colours! How about you draw a picture of a dragon flying through the sky") {
+// 	// Take the textInput recommending something to draw and connect to the LLM
+	
+// }
